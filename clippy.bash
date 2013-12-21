462469ec57141e728fe7e944d613613656a90ff4
Nathaniel Hellabyte <nate@hellabit.es>
2013-12-21 03:08:24 -0700

    :)

--- clippy.bash
+++ clippy.bash
@@ -1,1 +1,12 @@
-/usr/local/Athenaeum/manufactum/clippy.bash
+#!/usr/bin/env bash
+
+myhome=$(eval echo ~$(whoami))
+target=${myhome}/.clippy
+
+if [[ -p /dev/stdin ]]; then
+    cat > $target
+elif ! [[ -z $@ ]]; then
+    echo $@ > $target
+else
+    cat $target
+fi
